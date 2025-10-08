29276b68ee25971adb00bca13f1554054c33bc35
ChavadaHarsh <harshchavada@techmatrix.io>
2025-10-08 16:19:57 +0530

    about us page complete.

--- resources/js/components/PageLayout.vue
+++ resources/js/components/PageLayout.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="px0 pt-0 lg:pt-0 w-full h-auto">
-        <div class="w-[98%] md:w-[85%] lg:w-[85%] xl:w-[80%] h-auto m-auto">
+        <div class="w-[98%] md:w-[85%] lg:w-[85%] xl:w-[75%] h-auto m-auto">
             <slot />
         </div>
     </div>
